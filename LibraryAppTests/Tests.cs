d817fb57ebf8e6d278d2b774810e79ab32a16deb
Domotor.Bendegúz <bende.domotor@gmail.com>
2026-05-28 13:19:34 +0200

    FirstThreeTestMethods

--- LibraryAppTests/Tests.cs
+++ LibraryAppTests/Tests.cs
@@ -22,6 +22,12 @@ namespace LibraryAppTests
             Assert.AreEqual("City Library", lib.GetName());
         }
         // TODO: null vagy üres névvel létrehozva ArgumentException-t kell dobni
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_NullName()
+        {
+            var lib = new Library(null);
+        }
 
         // ---- AddBook ----
 
@@ -33,7 +39,22 @@ namespace LibraryAppTests
             Assert.AreEqual(1, lib.GetTotalTitles());
         }
         // TODO: ugyanazt a címet hozzáadva újabb bejegyzések kerülnek az _availableBooks listába, és GetTotalTitles nem változik
+        [TestMethod]
+        public void AddBook_ExistingTitle()
+        {
+            var lib = CreateDefaultLibrary();
+            lib.AddBook("Dune", 2);
+            Assert.AreEqual(1, lib.GetTotalTitles());
+            Assert.AreEqual(5, lib.GetAvailableCopies("Dune"));
+        }
         // TODO: copies értéke 0 vagy negatív esetén ArgumentException-t kell dobni
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddBook_InvalidCopies()
+        {
+            var lib = new Library("City Library");
+            lib.AddBook("Dune", 0);
+        }
 
         // ---- BorrowBook ----
 
@@ -46,7 +67,20 @@ namespace LibraryAppTests
             Assert.AreEqual(2, lib.GetAvailableCopies("Dune"));
         }
         // TODO: nem létező cím esetén false-t kell visszaadni és nem dob kivételt
+        [TestMethod]
+        public void BorrowBook_NonExistingTitle()
+        {
+            var lib = CreateDefaultLibrary();
+            Assert.IsFalse(lib.BorrowBook("NonExisting"));
+        }
         // TODO: az összes példány kikölcsönzése után újabb kölcsönzés false-t ad vissza
+        [TestMethod]
+        public void BorrowBook_AllCopiesBorrowed()
+        {
+            var lib = CreateDefaultLibrary();
+            Assert.IsTrue(lib.BorrowBook("1984"));
+            Assert.IsFalse(lib.BorrowBook("1984"));
+        }
 
         // ---- ReturnBook ----
 
@@ -60,6 +94,7 @@ namespace LibraryAppTests
             Assert.AreEqual(1, lib.GetAvailableCopies("1984"));
         }
         // TODO: nem létező cím visszahozásakor false-t kell visszaadni
+
         // TODO: olyan könyv visszahozásakor, amelyből semmi sincs kikölcsönzve, false-t kell adni
 
         // ---- GetAvailableCopies ----
